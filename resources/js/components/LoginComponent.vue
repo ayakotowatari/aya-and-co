91c1ac309c7b90d461154186d93387c0593337a6
Ayako Towatari <kyotayako@gmail.com>
2021-10-12 00:21:44 +0900

    Member Login added

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -4,7 +4,7 @@
             <v-col cols="12" sm="12" md="12">
                 <div class="heading-group">
                     <div class="page-heading">
-                        Login
+                        Member Login
                     </div>
                     <div class="page-subtitle grey--text text--darken-3">
                         会員ログイン
